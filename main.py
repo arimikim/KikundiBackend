4385edeb7501d6de7203c1d6aabadc3617a223ed
Arimi Mureti <arimivictor@gmail.com>
2026-01-19 18:12:19 +0300

    Adding the cloud postgl

--- main.py
+++ main.py
@@ -3,11 +3,14 @@ from sqlalchemy.orm import Session,declarative_base,sessionmaker
 from sqlalchemy import create_engine, Column, Integer, String,ForeignKey,DateTime
 from datetime import datetime, timedelta
 from pydantic import BaseModel
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 
-database_url = "sqlite:///./kikundi.db"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(database_url,connect_args={"check_same_thread": False})
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
